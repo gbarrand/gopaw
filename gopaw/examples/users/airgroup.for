428f2eadd66bc40cb87e600e0bbd38fc514e88fe
Guy Barrand <guy.barrand@gmail.com>
2020-06-27 16:06:56 +0200

    update

--- gopaw/examples/users/airgroup.for
+++
@@ -1,9 +0,0 @@
-      function airgroup(x)
-      real k0,k1,k2,k3
-      k0=238.0185
-      k1=5792105
-      k2=57.362
-      k3=167917
-      s=(x/1.24)**2
-      airgroup=(k1*(k0+s)/(k0-s)**2+k3*(k2+s)/(k2-s)**2)/1e5      
-      end
